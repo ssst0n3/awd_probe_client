60e9358f999cec9c6307f75c95279c09672306bd
st0n3 <ssst0n3@gmail.com>
2021-03-05 14:50:52 +0800

    update

--- test/app/app.go
+++ test/app/app.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	awd_probe_client.ProbeHost = "127.0.0.1:13500"
+	//awd_probe_client.ProbeHost = "127.0.0.1:13500"
 	r := gin.New()
 	r.Use(awd_probe_client.Proxy)
 	r.GET("/ping", func(context *gin.Context) {
