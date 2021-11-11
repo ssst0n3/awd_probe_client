332b818776e193a6dbc12c5651193d1b3188c2d9
Lei Wang <ssst0n3@gmail.com>
2021-11-11 15:07:28 +0800

    add beego

--- middleware.go
+++ middleware.go
@@ -3,32 +3,35 @@ package awd_probe_client
 import (
 	"bytes"
 	"context"
+	beegoContext "github.com/astaxie/beego/context"
 	"github.com/gin-gonic/gin"
 	"github.com/ssst0n3/awesome_libs/awesome_error"
 	"github.com/ssst0n3/awesome_libs/log"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
-func Proxy(c *gin.Context) {
-	if c.GetHeader("Probe-Repeater") == "yes" {
+func Proxy(request *http.Request, clientIp string) {
+	if request.Header.Get("Probe-Repeater") == "yes" {
 		return
 	}
-	body, err := ioutil.ReadAll(c.Request.Body)
+	body, err := ioutil.ReadAll(request.Body)
 	if err != nil {
 		awesome_error.CheckErr(err)
 		return
 	}
-	c.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
-	req := c.Request.Clone(context.TODO())
+	request.Body = ioutil.NopCloser(bytes.NewReader(body))
+	req := request.Clone(context.TODO())
 	req.RequestURI = ""
 	req.URL.Host = ProbeHost
 	req.URL.Scheme = "http"
-	if c.Request.TLS != nil {
+	if request.TLS != nil {
 		req.URL.Scheme = "https"
 	}
-	req.Header.Set("Probe-Forwarded-For", c.ClientIP())
+	req.Header.Set("Probe-Forwarded-For", clientIp)
 	req.Body = ioutil.NopCloser(bytes.NewReader(body))
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -37,4 +40,30 @@ func Proxy(c *gin.Context) {
 	}
 	dump, _ := httputil.DumpResponse(resp, true)
 	log.Logger.Debug(dump)
+}
+
+func Gin(c *gin.Context) {
+	Proxy(c.Request, c.ClientIP())
+}
+
+func getClientIp(req *http.Request) string {
+	clientIP := req.Header.Get("X-Forwarded-For")
+	clientIP = strings.TrimSpace(strings.Split(clientIP, ",")[0])
+	if clientIP == "" {
+		clientIP = strings.TrimSpace(req.Header.Get("X-Real-Ip"))
+	}
+	if clientIP != "" {
+		return clientIP
+	}
+	if addr := req.Header.Get("X-Appengine-Remote-Addr"); addr != "" {
+		return addr
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(req.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
+
+func Beego(c *beegoContext.Context) {
+	Proxy(c.Request, getClientIp(c.Request))
 }
